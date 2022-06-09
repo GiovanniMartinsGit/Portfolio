8243349f08da3ac359121679db71d5de7aae92af
GiovanniMartinsGit <gmgit10@gmail.com>
2022-06-09 09:16:55 -0300

    initial

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const btnMobile = document.getElementById('btn-menu');
+
+function toggleMenu(event) {
+    
+  if(event.type === 'touchstart') event.preventeDefault();
+
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
